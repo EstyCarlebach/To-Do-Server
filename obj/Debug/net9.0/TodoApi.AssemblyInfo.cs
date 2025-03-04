d1f417b6263a3c5e2a8052d3c7e460e7dc82f8f8
Esty Carlebach <e0583241655@gmail.com>
2025-03-04 16:33:44 +0200

    Resolved merge conflicts

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f445990f6fdab706166976705b2a041aa1f8a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
